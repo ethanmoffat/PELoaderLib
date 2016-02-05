9ff1a812fb141615f4dd38f80a87bdf7a4ad1ab8
Ethan Moffat <ethanmoffat@hotmail.com>
2016-02-04 18:33:17 -0800

    First Commit!

---
+++ PELoaderLib/IPEFile.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+
+namespace PELoaderLib
+{
+	public interface IPEFile : IDisposable
+	{
+		string FileName { get; }
+
+		bool Initialize();
+	}
+}
---
+++ PELoaderLib/PEFile.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System.IO.MemoryMappedFiles;
+
+namespace PELoaderLib
+{
+	public class PEFile : IPEFile
+	{
+		public string FileName { get; private set; }
+
+		private MemoryMappedFile _file;
+		private MemoryMappedViewStream _fileStream;
+
+		public PEFile(string filename)
+		{
+			FileName = filename;
+			CreateFileStreams();
+		}
+
+		public bool Initialize()
+		{
+			return false;
+		}
+
+		private void CreateFileStreams()
+		{
+			_file = MemoryMappedFile.CreateFromFile(FileName);
+			_fileStream = _file.CreateViewStream();
+		}
+
+		#region IDisposable
+
+		~PEFile()
+		{
+			Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (_fileStream != null)
+					_fileStream.Dispose();
+				_fileStream = null;
+
+				if (_file != null)
+					_file.Dispose();
+				_file = null;
+			}
+		}
+
+		#endregion
+	}
+}
